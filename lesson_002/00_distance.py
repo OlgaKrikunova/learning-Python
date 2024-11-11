498b4e45dd2bd851e542d8ab1e74ce1d60a5384e
Oleg Sergeevich <olgakrikunova12@gmail.com>
2024-11-11 17:38:26 +0300

    domashka 1

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -5,6 +5,7 @@
 
 from pprint import pprint
 
+
 sites = {
     'Moscow': (550, 370),
     'London': (510, 510),
@@ -14,24 +15,33 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
-distances = {}
+distances = dict()
 
 moscow = sites['Moscow']
 london = sites['London']
 paris = sites['Paris']
 
-moscow_london = ((moscow[0] - london[0]) ** 2 + (moscow[1] - london[1]) ** 2) ** .5
-moscow_paris = ((moscow[0] - paris[0]) ** 2 + (moscow[1] - paris[1]) ** 2) ** .5
-london_paris = ((london[0] - paris[0]) ** 2 + (london[1] - paris[1]) ** 2) ** .5
+moscow_london = ((moscow[0] - london[0])**2 + (moscow[1] - london[1])**2) ** .5
+moscow_paris = ((moscow[0] - paris[0])**2 + (moscow[1] - paris[1])**2) ** .5
 
 distances['Moscow'] = {}
 distances['Moscow']['London'] = moscow_london
 distances['Moscow']['Paris'] = moscow_paris
 
-distances['London'] = {}
-distances['London']['paris'] = london_paris
-distances['London']['Moscow'] = moscow_london
+london_paris = ((london[0] - paris[0])**2 + (london[1] - paris[1])**2) ** .5
+london_moscow = ((london[0] - moscow[0])**2 + (london[1] - moscow[1])**2) ** .5
 
-# TODO здесь заполнение словаря
+distances['London'] = {}
+distances['London']['Paris'] = london_paris
+distances['London']['Moscow'] = london_moscow
+
+paris_london = ((paris[0] - london[0])**2 + (paris[1] - london[1])**2) ** .5
+paris_moscow = ((paris[0] - moscow[0])**2 + (paris[1] - moscow[1])**2) ** .5
+
+distances['Paris'] = {}
+distances['Paris']['London'] = paris_london
+distances['Paris']['Moscow'] = paris_moscow
 
 pprint(distances)
+
+pprint(distances['Paris']['London'])
