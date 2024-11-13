36def60b24e382ad4515091ef73760d45f4d3119
Oleg Sergeevich <olgakrikunova12@gmail.com>
2024-11-13 16:08:25 +0300

    domashka 7 songs

--- lesson_002/06_songs_list.py
+++ lesson_002/06_songs_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Есть список песен группы Depeche Mode со временем звучания с точносттю до долей минут
+# Есть список песен группы Depeche Mode со временем звучания с точностью до долей минут
 
 violator_songs_list = [
     ['World in My Eyes', 4.86],
@@ -15,12 +15,17 @@ violator_songs_list = [
     ['Clean', 5.83],
 ]
 
-# распечатайте общее время звучания трех песен: 'Halo', 'Enjoy the Silence' и 'Clean' в формате
+# Распечатайте общее время звучания трех песен: 'Halo', 'Enjoy the Silence' и 'Clean' в формате
 #   Три песни звучат ХХХ минут
-# Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
+# обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
-# TODO здесь ваш код
+song = round(violator_songs_list[3][1]+violator_songs_list[5][1]+violator_songs_list[8][1], 2)
+print(song)
+
+song_2 = f'три песни звучат {song}'
+print(song_2)
+
 
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
@@ -38,4 +43,10 @@ violator_songs_dict = {
 # распечатайте общее время звучания трех песен: 'Sweetest Perfection', 'Policy of Truth' и 'Blue Dress'
 #   А другие три песни звучат ХХХ минут
 
-# TODO здесь ваш код
+time = round(violator_songs_dict['Sweetest Perfection'] + violator_songs_dict['Policy of Truth']
+        + violator_songs_dict['Blue Dress'], 2)
+
+time_2 = f'А другие три песни звучат {time}'
+print(time)
+
+print(time_2)
