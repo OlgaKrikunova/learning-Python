1fe3e748989fa5632366f763f0442f1fdfb6be95
Oleg Sergeevich <olgakrikunova12@gmail.com>
2024-11-29 13:52:35 +0400

    domaschka 3 - rainbow, wall, smile

--- Lesson_3/06_rainbow.py
+++ Lesson_3/06_rainbow.py
@@ -8,11 +8,25 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
-# TODO здесь ваш код
+
+# y = 350
+#
+# for color in rainbow_colors:
+#     sd.line(start_point=sd.get_point(50,y), end_point=sd.get_point(500, y), width=5, color=color)
+#     y += 5
+
+
 
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
-# TODO здесь ваш код
+
+y = 150
+for color_1 in rainbow_colors:
+    sd.circle(center_position=sd.get_point(300, y), radius=350, width=5, color=color_1)
+    y +=5
+
+
+
 
 sd.pause()
--- Lesson_3/08_smile.py
+++ Lesson_3/08_smile.py
@@ -1,13 +1,56 @@
 # -*- coding: utf-8 -*-
+from random import randint
 
 # (определение функций)
-import simple_draw
+import simple_draw as sd
 
 # Написать функцию отрисовки смайлика в произвольной точке экрана
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
 
+
+sd.resolution = (1200, 600)
+
+def r_eya(x, y):
+    radius = 10
+    x += 35
+    y += 20
+    point = sd.get_point(x, y)
+    sd.circle(center_position=point, radius=radius, width=5)
+
+def l_eya(x, y):
+    radius = 10
+    x -= 35
+    y += 20
+    point = sd.get_point(x, y)
+    sd.circle(center_position=point, radius=radius, width=5)
+
+def bubble(x, y):
+    radius = 70
+    point = sd.get_point(x, y)
+    sd.circle(center_position=point, radius=radius, width=5)
+    r_eya(x,y)
+    l_eya(x,y)
+
+    a = x - 10
+    b = y - 15
+    a_1 = x + 15
+    b_1 = y - 15
+    point = sd.get_point(a, b)
+    point_1 = sd.get_point(a_1, b_1)
+    sd.line(start_point=point, end_point=point_1, width=50)
+
+
+for _ in range(5):
+    x = randint(100, 1000)
+    y = randint(100, 500)
+    bubble(x,y)
+
+
+
+
+
 # TODO здесь ваш код
 
-simple_draw.pause()
+sd.pause()
