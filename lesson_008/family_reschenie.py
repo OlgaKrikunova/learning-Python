83adb01feb1efc64c7a6b5845173c866c5f8f3a0
Oleg Sergeevich <olgakrikunova12@gmail.com>
2025-01-30 17:20:42 +0400

    lesson 8 - family - added a cat

--- lesson_008/family_reschenie.py
+++ lesson_008/family_reschenie.py
@@ -7,20 +7,22 @@ class House:
     def __init__(self):
         self.money = 100
         self.food = 50
+        self.cat_food = 30
         self.dirt_in_house = 0
         self.coats = 0
         self.geld = 0
         self.foods = 0
 
     def __str__(self):
-        return 'В доме еды осталось {}, денег осталось {}, грязи {}'.format(
-            self.food, self.money, self.dirt_in_house)
+        return ('В доме еды осталось {}, денег осталось {}, грязи {}, '
+                'еды кошака осталось {}').format(self.food, self.money,
+                                                 self.dirt_in_house, self.cat_food)
 
 
 home = House()
 
 
-class People:
+class Family:
     food = 0
 
     def __init__(self, name):
@@ -37,15 +39,15 @@ class People:
             self.house.dirt_in_house += 5
             self.house.foods += 20
 
-
         else:
             print('{} нет еды'.format(self.name))
 
     def __str__(self):
-        return 'Я - {}, сытость - {}, счастье - {}'.format(self.name, self.fullness, self.happiness)
+        return 'Я - {}, сытость - {}, счастье - {}'.format(
+            self.name, self.fullness, self.happiness)
 
 
-class Husband(People):
+class Husband(Family):
 
     def __init__(self, name, profession):
         super().__init__(name)
@@ -67,6 +69,10 @@ class Husband(People):
         self.happiness += 20
         self.house.dirt_in_house += 5
 
+    def pet_cat(self):
+        print('{} погладил кота'.format(self.name))
+        self.happiness += 5
+
     def act(self):
         if self.fullness < 10:
             print('{} умер...'.format(self.name))
@@ -74,15 +80,18 @@ class Husband(People):
         if self.happiness < 10:
             print('{} умер...'.format(self.name))
             return
-        if self.fullness < 20:
+        dice = randint(1, 6)
+        if self.fullness <= 20:
             self.eat()
         elif self.house.money <= 400:
             self.work()
-        else:
+        elif self.happiness >= 10:
+            self.pet_cat()
+        elif dice == 6:
             self.gaming()
 
 
-class Wife(People):
+class Wife(Family):
 
     def __init__(self, name):
         super().__init__(name)
@@ -96,10 +105,12 @@ class Wife(People):
             self.house.food += 60
             self.house.money -= 60
             self.fullness -= 10
+            self.happiness -= 5
 
-
-        else:
-            print('{} деньги кончились!'.format(self.name))
+        if self.house.money > 70:
+            print('{} сходила в магазин за едой кошаку.'.format(self.name))
+            self.house.cat_food += 20
+            self.house.money -= 20
 
     def buy_fur_coat(self):
         if self.house.money > 350:
@@ -116,6 +127,10 @@ class Wife(People):
             self.fullness -= 10
             self.happiness -= 10
 
+    def pet_cat(self):
+        print('{} погладил кота'.format(self.name))
+        self.happiness += 5
+
     def act(self):
         if self.fullness < 10:
             print('{} умер...'.format(self.name))
@@ -131,17 +146,59 @@ class Wife(People):
             self.clean_house()
         elif self.house.food <= 10:
             self.shopping()
+        elif self.house.cat_food <= 10:
+            self.shopping()
+        dice = randint(1, 8)
+        if dice == 1:
+            self.pet_cat()
+
+
+class Cat(Family):
+
+    def __str__(self):
+        return super().__str__()
+
+    def eat(self):
+        if self.house.cat_food > 10:
+            print('{} поел'.format(self.name))
+            self.fullness += 20
+            self.house.cat_food -= 10
+
+    def slip(self):
+        if self.fullness > 10:
+            print('{} спал'.format(self.name))
+            self.fullness -= 10
+
+    def wallpaper(self):
+        if self.fullness > 10:
+            print('{} дерет обои'.format(self.name))
+            self.fullness -= 10
+            self.house.dirt_in_house += 5
+
+    def act(self):
+        if self.fullness < 10:
+            print('{} умер...'.format(self.name))
+            return
+        if self.fullness <= 10:
+            self.eat()
+        elif self.fullness > 30:
+            self.slip()
+        else:
+            self.wallpaper()
 
 
 serge = Husband(name='Сережа', profession="айтишник")
 masha = Wife(name='Маша')
+cit = Cat(name='Котэ')
 
 for day in range(1, 366):
     print('================== День {} =================='.format(day))
     serge.act()
     masha.act()
+    cit.act()
     print(serge)
     print(masha)
+    print(cit)
     print(home)
 print()
 print('Всего заработано денег {}, всего съедено еды {},'
